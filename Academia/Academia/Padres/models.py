47ac4dafd04e7f11c3140355f1b856657ee1bfe2
Alvaro <alvarobl@libera-t.com>
2021-09-08 19:05:00 +0200

    añadido caracteristicas al models Padre

--- Academia/Academia/Padres/models.py
+++ Academia/Academia/Padres/models.py
@@ -5,6 +5,9 @@ class Padres(models.Model):
     id = models.IntegerField(primary_key=True)
     nombre= models.CharField(max_length=30)
     apellidos = models.CharField(max_length=60)
+    telefono = models.CharField(max_length=30)
     dni = models.CharField(max_length=12)
     mail = models.CharField(max_length=100)
+    #pagos = models.ForeignKey('Pagos.models.Pagos', on_delete=CASCADE)
+
 
